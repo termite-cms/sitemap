2ac803927d58f0031e3c50a933523259ecf73aab
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-06-29 20:55:23 +0200

    Page should be an observable

--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -3,10 +3,11 @@
 namespace Termite\Sitemap;
 
 use React\Promise\PromiseInterface;
+use Rx\Observable;
 
 interface HandlerInterface
 {
     public function count(): PromiseInterface;
 
-    public function page(int $page): PromiseInterface;
+    public function page(int $page): Observable;
 }
